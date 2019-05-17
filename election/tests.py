2e5f7c975b1bca8b5eb54e7b32edc4dfc206689b
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2019-05-17 14:04:47 +0200

    Test for election creation
    ->Create a an election with title.
    ->Get back the id of the created election
    ->Get the election back and check the title is the same

--- election/tests.py
+++ election/tests.py
@@ -3,15 +3,24 @@ from rest_framework.test import APITestCase
 from election.models import Election
 import election.urls as urls
 
+import json
 
 class ElectionCreateAPIViewTestCase(APITestCase):
 
     def test_create_election(self):
+        title = "Super élection - utf-8 chars: 🤨 😐 😑 😶 🙄 😏 😣 😥 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 ☹️ 🙁 😖 😞 😟 😤 😢 😭 😦 😧 😨 😩 🤯 !"
+
         response_post = self.client.post(
             urls.new_election(),
-            {"title": "testing election/create !"},
+            {"title": title},
         )
         self.assertEqual(201, response_post.status_code)
+
+        election_pk = response_post.data["id"]
+        response_get = self.client.get(urls.election_details(election_pk))
+        self.assertEqual(200, response_get.status_code)
+        self.assertEqual(title, response_get.data["title"])
+
 
 
 class ElectionDetailsAPIViewTestCase(APITestCase):
