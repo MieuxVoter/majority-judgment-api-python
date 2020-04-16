287c32bebc32e6e851178de6986d913fd6d9b6fe
guhur <12297742+guhur@users.noreply.github.com>
2020-04-16 17:22:21 +0200

    Seb/send invitation (#25)
    * First version of sending better mail (#17)
    
    * clean docker launch
    
    * add license for wait-for-it
    
    * Add environment variables
    
    Variables added: SECRET_KEY, DJANGO_ALLOWED_HOSTS, DJANGO_DEBUG.
    
    * fix r361682046
    
    * Update requirements.txt
    
    Update to fixed and recent versions
    
    * Update README.md
    
    * Update README.md
    
    Update path for tests
    
    * test
    
    Co-authored-by: guhur <12297742+guhur@users.noreply.github.com>
    
    * A new version of sending mail. (#19)
    
    * clean docker launch
    
    * add license for wait-for-it
    
    * Add environment variables
    
    Variables added: SECRET_KEY, DJANGO_ALLOWED_HOSTS, DJANGO_DEBUG.
    
    * fix r361682046
    
    * Update requirements.txt
    
    Update to fixed and recent versions
    
    * Update README.md
    
    * Update README.md
    
    Update path for tests
    
    * txt par gmail
    
    * Send mail + language templates
    
    * Update .env
    
    * Update 0011_election_selec_language.py
    
    * Send mail + languages templates
    
    * Send mail + languages templates
    
    Co-authored-by: guhur <12297742+guhur@users.noreply.github.com>
    
    * election start end (#22)
    
    * election start end
    
    * election start end
    
    * fix bug on restrict_results
    
    * remove cors
    
    * clean migrations
    
    * freeze token feature
    
    Co-authored-by: HermessNRJ <remi.boidet@gmail.com>

--- election/models.py
+++ election/models.py
@@ -1,36 +1,31 @@
 import logging
-
+from time import time
 from django.contrib.postgres.fields import ArrayField
 from django.db import IntegrityError, models
 from django.conf import settings
-
 from libs.django_randomprimary import RandomPrimaryIdModel
 
 logger = logging.getLogger(__name__)
+
 
 class Election(RandomPrimaryIdModel):
 
     title = models.CharField("Title", max_length=255)
     candidates = ArrayField(models.CharField("Name", max_length=255))
     on_invitation_only = models.BooleanField(default=False)
-
-    # An opened election is Doodle-like: results are always visible
-    is_opened = models.BooleanField(default=True)
-    is_finished = models.BooleanField(default=False)
-    is_started = models.BooleanField(default=True)
     num_grades = models.PositiveSmallIntegerField("Num. grades", null=False)
+    start_at = models.IntegerField("Start date", default=round(time()))
+    finish_at = models.IntegerField("End date",default=round(time()+1))
+    # Language preference is used for emailing voters
+    select_language = models.CharField("Language", max_length=2,default="en")
+    # An opened election is Doodle-like: results are always visible
+    restrict_results = models.BooleanField(default=True)
 
-    #Language selection (French by default)
-    selec_language = models.CharField("Language", max_length=2,default="fr")
-
-    # make sure we don't ask for more grades than allowed in the database
+    # add some constraints before saving the database
     def save(self, *args, **kwargs):
-
+        # make sure we don't ask for more grades than allowed in the database
         if self.num_grades is None:
             raise IntegrityError("Election requires a positive number of grades.")
-
-        if self.title is None or self.title == "":
-            raise IntegrityError("Election requires a proper title")
 
         if self.num_grades > settings.MAX_NUM_GRADES or self.num_grades <= 0:
             raise IntegrityError(
@@ -38,7 +33,12 @@ class Election(RandomPrimaryIdModel):
                 % (self.num_grades, settings.MAX_NUM_GRADES)
             )
 
-        if not self.selec_language in settings.LANGUAGE_AVAILABLE:
+        # check that the title is not empty            
+        if self.title is None or self.title == "":
+            raise IntegrityError("Election requires a proper title")
+
+        # check that the language is known
+        if not self.select_language in settings.LANGUAGE_AVAILABLE:
             string_language =  ', '.join(settings.LANGUAGE_AVAILABLE)
             raise IntegrityError("Election is only available in " + string_language) 
 
--- election/views.py
+++ election/views.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from time import time
 from django.db import IntegrityError
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
@@ -10,27 +12,42 @@ from rest_framework.views import APIView
 import election.serializers as serializers
 from election.models import Election, Token, Vote
 from libs import majority_judgment as mj
-
+ 
 # Error codes:
 UNKNOWN_ELECTION_ERROR = "E1: Unknown election"
 ONGOING_ELECTION_ERROR = "E2: Ongoing election"
 NO_VOTE_ERROR = "E3: No recorded vote"
+ELECTION_NOT_STARTED_ERROR = "E4: Election not started"
+ELECTION_FINISHED_ERROR = "E5: Election finished"
+INVITATION_ONLY_ERROR = "E6: Election on invitation only, please provide token"
+UNKNOWN_TOKEN_ERROR = "E7: Wrong token"
+USED_TOKEN_ERROR = "E8: Token already used"
+WRONG_ELECTION_ERROR = "E9: Parameters for the election are incorrect"
 
-def send_mail_invitation(email, election):   
+
+def send_mail_invitation(email: str, election: str, token_id: Optional[int] = None):   
+    token_get: str = "?token={token_id}" if token_id is not None else ""
     merge_data = {
-        "invitation_url":settings.SITE_URL + "/vote/" + election.id,
-        "result_url":settings.SITE_URL + "/result/" + election.id,
+        "invitation_url": f"{settings.SITE_URL}/vote/{election.id}{token_get}",
+        "result_url": settings.SITE_URL + "/result/" + election.id,
         "title": election.title,
         }
-    text_body = render_to_string("election/"+ election.selec_language +"_mail_invitation.txt",merge_data)
-    html_body = render_to_string("election/"+ election.selec_language +"_mail_invitation.html",merge_data)   
+    text_body = render_to_string(
+        "election/"+ election.select_language +"_mail_invitation.txt",
+        merge_data
+    )
+    html_body = render_to_string(
+            "election/"+ election.select_language +"_mail_invitation.html",
+            merge_data
+    )   
     msg = EmailMultiAlternatives(
         election.title,
         text_body,
         settings.EMAIL_HOST_USER,
-        [ email ])
+        [email])
     msg.attach_alternative(html_body, "text/html")
     msg.send()
+
 
 class ElectionCreateAPIView(CreateAPIView):
     serializer_class = serializers.ElectionCreateSerializer
@@ -47,11 +64,11 @@ class ElectionCreateAPIView(CreateAPIView):
                     election=election,
                     email=email,
                 )
-                print(# TODO!
+                print(
                     "Send mail : id election: %s, token: %s, email: %s"
                     %(election.id, token.id, email)
                 )
-                send_mail_invitation(email, election)
+                send_mail_invitation(email, election, token_id=token.id)
             else:
                 send_mail_invitation(email, election)
 
@@ -61,6 +78,35 @@ class ElectionCreateAPIView(CreateAPIView):
 
 class ElectionDetailsAPIView(RetrieveAPIView):
     serializer_class = serializers.ElectionViewSerializer
+
+    def get(self, request, pk, **kwargs):
+
+        try:
+            election = Election.objects.get(id=pk)
+        except Election.DoesNotExist:
+            return Response(
+                UNKNOWN_ELECTION_ERROR,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if round(time()) < election.start_at:
+            return Response(
+                ELECTION_NOT_STARTED_ERROR,
+                status=status.HTTP_401_UNAUTHORIZED,                
+            
+            )
+        if round(time()) >= election.finish_at:
+            return Response(
+                ELECTION_FINISHED_ERROR,
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        
+        serializer = serializers.ElectionViewSerializer(election)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
+        
     queryset = Election.objects.all()
 
 
@@ -73,12 +119,14 @@ class VoteAPIView(CreateAPIView):
 
         election = serializer.validated_data["election"]
 
-        if election.on_invitation_only:
+        # TODO this feature is not yet implement in the front 
+        # if election.on_invitation_only:
+        if False:
             try:
                 token = serializer.validated_data["token"]
             except KeyError:
                 return Response(
-                    "election on invitation only, please provide token",
+                    INVITATION_ONLY_ERROR,
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
@@ -89,13 +137,13 @@ class VoteAPIView(CreateAPIView):
                 )
             except Token.DoesNotExist:
                 return Response(
-                    "wrong token",
+                    UNKNOWN_TOKEN_ERROR,
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
             if token_object.used:
                 return Response(
-                    "token already used",
+                    USED_TOKEN_ERROR,
                     status=status.HTTP_400_BAD_REQUEST,
                 )
             token_object.used = True
@@ -107,7 +155,10 @@ class VoteAPIView(CreateAPIView):
         try:
             self.perform_create(serializer)
         except IntegrityError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                WRONG_ELECTION_ERROR,
+                status=status.HTTP_400_BAD_REQUEST
+            )
         headers = self.get_success_headers(serializer.data)
         return Response(status=status.HTTP_201_CREATED, headers=headers)
 
@@ -119,7 +170,6 @@ class ResultAPIView(APIView):
     View to list the result of an election using majority judgment.
     """
 
-
     def get(self, request, pk, **kwargs):
 
         try:
@@ -129,8 +179,13 @@ class ResultAPIView(APIView):
                 UNKNOWN_ELECTION_ERROR,
                 status=status.HTTP_400_BAD_REQUEST,
             )
+        except IntegrityError:
+            return Response(
+                WRONG_ELECTION_ERROR,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        if not election.is_finished and not election.is_opened:
+        if (election.restrict_results and round(time()) < election.finish_at):
             return Response(
                 ONGOING_ELECTION_ERROR,
                 status=status.HTTP_400_BAD_REQUEST,
@@ -144,11 +199,14 @@ class ResultAPIView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        profiles, scores, grades = mj.compute_votes([v.grades_by_candidate for v in votes],
-                                    election.num_grades)
+        profiles, scores, grades = mj.compute_votes(
+            [v.grades_by_candidate for v in votes],
+            election.num_grades
+        )
         sorted_indexes = mj.majority_judgment(profiles)
-        #grades = [mj.majority_grade(profile) for profile in profiles]
-        candidates = [serializers.Candidate(election.candidates[idx], idx, p, g, s)
-                      for idx, p, s, g in zip(sorted_indexes, profiles, scores, grades)]
+        candidates = [
+            serializers.Candidate(election.candidates[idx], idx, p, g, s)
+            for idx, p, s, g in zip(sorted_indexes, profiles, scores, grades)
+        ]
         serializer = serializers.CandidateSerializer(candidates, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
