b525558049919a3398e1366541c1956bd8f60308
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2020-04-26 13:58:16 +0200

    [DASH] fix election graph

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+from typing import List, Dict
 import os
 import pathlib
-
+import django
+from django.db.models import Count, F
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,6 +12,16 @@ import dash_table
 import plotly.graph_objs as go
 import dash_daq as daq
 import dash_bootstrap_components as dbc
+
+def load_mvapi():
+    import os
+    import sys
+    sys.path.append('../')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mvapi.settings")
+    django.setup()
+
+load_mvapi()
+from election.models import Election, Vote, Token
 
 
 def build_banner(app: dash.Dash):
@@ -52,35 +65,99 @@ def build_banner(app: dash.Dash):
     )
 
     
-def build_quick_stats_panel(num_elections: int, num_votes: int):
+def led_display(title, value):
+    return html.Div(
+        children=[
+            html.P(title),
+            daq.LEDDisplay(
+                value=value,
+                color="#92e0d3",
+                backgroundColor="#1e2130",
+                size=50,
+            ),
+        ],
+    )
+
+def build_quick_stats_panel():
+    num_elections: int = len(Election.objects.all())
+    num_votes: int = len(Vote.objects.all())
+
     return dbc.Col([
-        dbc.Row([dbc.Col([html.Div(
-                id="card-1",
-                children=[
-                    html.P("Number of elections"),
-                    daq.LEDDisplay(
-                        id="num-elections",
-                        value=num_elections,
-                        color="#92e0d3",
-                        backgroundColor="#1e2130",
-                        size=50,
-                    ),
-                ],
-            )])]),
         dbc.Row([
-        dbc.Col([html.Div(
-                id="card-2",
-                children=[
-                    html.P("Number of votes"),
-                    daq.LEDDisplay(
-                        id="num-votes",
-                        value=num_votes,
-                        color="#92e0d3",
-                        backgroundColor="#1e2130",
-                        size=50,
-                    ),
-                ],
-            )]),
-    ])])
+            dbc.Col([
+                led_display("Number of elections", num_elections)
+            ])
+        ]),
+        dbc.Row([
+            dbc.Col([
+               led_display("Number of votes", num_votes)
+            ])
+        ])
+    ])
 
 
+
+def build_day_election_figure():
+    days_qs = (
+        Election
+        .objects
+        .all()
+        .annotate(start_day=F("start_at") / (24*3600))
+        .values("start_day")
+        .annotate(count=Count("start_day"))
+    )
+
+    if len(days_qs) == 0:
+        return html.H1(children=["No elections were found"])
+
+    start_days: Dict[int, int] = {}
+    for day in days_qs:
+        start_days[day["start_day"]] = day["count"]
+
+    all_days = range(
+            min(start_days.keys()),
+            int(datetime.now().timestamp() / 24 / 3600) + 1
+    )
+
+    day_names: Dict[str, int] = {
+        datetime
+        .fromtimestamp(timestamp * 24 * 3600)
+        .strftime("%Y/%m/%d"):
+            start_days.get(timestamp, 0)
+        for timestamp in all_days
+    }
+
+    return dcc.Graph(
+        figure=go.Figure(
+            data=[go.Bar(x=list(day_names.keys()), y=list(day_names.values()))],
+            layout=go.Layout(
+                title="Number of elections created per day"
+            )
+        )
+    )
+
+
+def build_most_voted_tables(top=10):
+    votes_qs = list(
+        Vote
+        .objects
+        .all()
+        .values('election_id', 'election__title')
+        .annotate(count=Count("election_id"))
+        .order_by("-count")
+        [:top]
+    )
+
+    if len(votes_qs) == 0:
+        return html.H1(children=["No votes were found"])
+
+    titles = [election["election__title"] for election in votes_per_el]
+    num_votes = [election["count"] for election in votes_per_el]
+    return dcc.Graph(
+        go.Figure(
+            data=[go.Table(
+                header=dict(values=["Title", "Number of votes"]),
+                cells=dict(values=[titles, num_votes]),
+            )]
+        )
+    )
--- docker/dash.sh
+++ docker/dash.sh
@@ -1,7 +1,4 @@
 pip install dash dash_daq dash-bootstrap-components
-echo "FOOO"
-ls /code/*
-echo .env.local
 pushd dashboard
 	python dash_app.py
 popd
