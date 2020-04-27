b25b0625d41189504462471c18ccd749352a101c
domi41 <dominique.merle.41@gmail.com>
2020-04-27 19:20:19 +0200

    chore(review)

--- tests/acceptance/environment.py
+++ tests/acceptance/environment.py
@@ -3,29 +3,21 @@ Environment module for acceptance testing of the scenaristic constitution.
 https://behave.readthedocs.io/en/latest/api.html#environment-file-functions
 """
 
-
-# Since we expect most of our step defs to require the flexibity of regexes,
-# we make regular expressions the default.  (I18N, epicene)
+# Since we expect most of our step defs to require the flexibility of regular
+# expressions (I18N, epicene) we make regular expressions the default.
 # This is at the expense of automatic type casting of step variables.
-# It's a somewhat good tradeoff since natural language numbers still do require
-# explicit casting in all matchers (AFAIK in early 2020).
 from behave import use_step_matcher
 use_step_matcher("re")
 # You can still override this right before your step def if you want,
 # by calling use_step_matcher() with one of the following:
 # "parse" (factory default), "cfparse"
-# and calling it again with "re" after your step def. (it uses a `global`)
-
-
-# def _log(*args):
-#     print(*args)  # hook to logger instead
+# and calling it again with "re" after your step def.  (it uses a `global`)
 
 
 def before_feature(context, feature):
     """
     Ran before _each_ feature file is exercised.
     """
-    # _log("Before feature", feature)
     pass
     # context.fixtures = ['behave-fixtures.json']
 
@@ -34,7 +26,6 @@ def before_scenario(context, scenario):
     """
     Ran before _each_ scenario is run.
     """
-    # _log("Before scenario", scenario)
     pass
     # context.fixtures.append('behave-second-fixture.json')
 
@@ -47,5 +38,4 @@ def before_all(context):
 
 
 def django_ready(context):
-    # _log("Django Ready", context)
     context.django = True
