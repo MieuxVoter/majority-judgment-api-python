f2a7bf67be2bba1560875e16e63c9bbb0b5e2c8d
domi41 <dominique.merle.41@gmail.com>
2020-04-29 07:40:14 +0200

    feat(feature): add a tool to find a user
    This may eventually support email and id.

--- tests/acceptance/steps/tools_dbal.py
+++ tests/acceptance/steps/tools_dbal.py
@@ -15,3 +15,8 @@ def count_polls():  # TBD: "scrutin" translates to "poll"?
     return Election.objects.count()
 
 
+def find_user(identifier):
+    user = User.objects.get(username=identifier)
+    if user is not None:
+        return user
+    raise ValueError("No user found matching `%s`." % identifier)
