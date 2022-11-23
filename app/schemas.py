4c733154cfe426958da67e5bddbdc00636741e4f
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-11-23 15:32:45 +0100

    fix: add order field

--- app/schemas.py
+++ app/schemas.py
@@ -140,6 +140,7 @@ class ElectionBase(BaseModel):
     hide_results: bool = True
     force_close: bool = False
     private: bool = False
+    random_order: bool = False
 
     _valid_date = _causal_dates_validator("date_created", "date_modified")
     _valid_date = _causal_dates_validator("date_start", "date_end")
