6556fb3791ba9e192ad7cc441df94bd862683c56
Théo Sabattié <theo.sabattie@gmail.com>
2025-06-14 01:08:05 +0200

    Added: migration file for auth_for_result

--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import pool
 from alembic import context
 from app.settings import settings
+from app.models import Base
 
 
 # this is the Alembic Config object, which provides
@@ -18,13 +19,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-db_url = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@l{settings.postgres_host}/{settings.postgres_name}"
+db_url = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}/{settings.postgres_name}"
 
 
 def run_migrations_offline() -> None:
